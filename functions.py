d5f178c4da29ec8abbbccf2e2584ce14547bc590
Vishal Panjeta <vishalpanjeta@gmail.com>
2016-12-25 13:51:41 +0530

    added functions

--- functions.py
+++ functions.py
@@ -6,3 +6,20 @@ def sigmoid(x):
 
 def dsigmoid(x):
     return x * (1. - x)
+
+def gaussian(x, mean=0.0, scale=1.0):
+    s = 2 * numpy.power(scale, 2)
+    e = numpy.exp( - numpy.power((x - mean), 2) / s )
+    return e / numpy.square(numpy.pi * s)
+
+def tanh(x):
+    return numpy.tanh(x)
+
+def dtanh(x):
+    return 1. - x * x
+
+def RELU(x):
+    return x * (x > 0)
+
+def dRELU(x):
+    return 1. * (x > 0)
