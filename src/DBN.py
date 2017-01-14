972285e70da3a2c7e47962ab777a93c18b1114cc
Vishal Panjeta <vishalpanjeta@gmail.com>
2017-01-14 21:05:23 +0530

    removed small bug

--- src/DBN.py
+++ src/DBN.py
@@ -104,8 +104,7 @@ class DBN:
 
 
 
-def test_dbn(pretrain_lr=0.1, pretraining_epochs=1000, k=1, \
-             finetune_lr=0.1, finetune_epochs=200):
+def test_dbn(pretrain_lr=0.1, pretraining_epochs=1000, k=1, finetune_lr=0.1, finetune_epochs=200):
 
     x = numpy.array([[1,1,1,0,0,0],
                      [1,0,1,0,0,0],
