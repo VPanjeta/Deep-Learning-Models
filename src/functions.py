567a1d6d0d8021a6d6246ac83e9f3156e30c0214
Vishal Panjeta <vishalpanjeta@gmail.com>
2017-01-24 22:23:53 +0530

    tweaked for CNNs

--- src/functions.py
+++ src/functions.py
@@ -31,10 +31,38 @@ def dReLU(x):
     return 1. * (x > 0)
 
 
-# # probability density for the Gaussian dist
-# def gaussian(x, mean=0.0, scale=1.0):
-#     s = 2 * numpy.power(scale, 2)
-#     e = numpy.exp( - numpy.power((x - mean), 2) / s )
+def create_demo_data(N_each, channel, n_in, n_out, rng, p=0.9):
+    if rng is None:
+        rng = numpy.random.RandomState(1234)
 
-#     return e / numpy.square(numpy.pi * s)
+    data = numpy.zeros( (N_each * n_out, channel, n_in, n_in) )
+    label = numpy.zeros( (N_each * n_out, n_out) )
 
+    K = n_in / n_out
+
+    index = 0
+    for k in xrange(n_out):  # for each class        
+        for num in xrange(N_each):  # for each sub data
+            for c in xrange(channel):
+                for i in xrange(n_in):
+                    for j in xrange(n_in):                
+
+                        if i < (k+1) * K and i >= k * K:
+                            # a = int(128 * rng.rand() + 128) * rng.binomial(size=1, n=1, p=p) / 256.0
+                            a = 128.0 * rng.binomial(size=1, n=1, p=p) / 256.0
+
+                        else:
+                            a = 128.0 * rng.binomial(size=1, n=1, p=1-p) / 256.0
+
+
+                        data[index][c][i][j] = a
+
+            for i in xrange(n_out):
+                if i == k:
+                    label[index][i] = 1.0
+                else:
+                    label[index][i] = 0.0
+
+            index += 1
+
+    return data, label
